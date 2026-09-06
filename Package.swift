d54a47bb458583c0d2ec7ea6b2c286cfb9873924
Brill Pappin <3903833+bpappin@users.noreply.github.com>
2026-09-06 09:38:04 -0400

    Bootstrap for initial push: set 0.0.1-alpha0 and disable publish workflow
    - Set version to 0.0.1-alpha0 for the initial GitHub push (alpha1 will be the first published release)
    - Disable publish.yml (rename to .disabled) so pushing master creates no release and publishes nothing

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PhoneNumber",
-            url: "https://github.com/aughtone/aughtone-phonenumber/releases/download/v0.0.1-alpha3/PhoneNumber.xcframework.zip",
+            url: "https://github.com/aughtone/aughtone-phonenumber/releases/download/v0.0.1-alpha1/PhoneNumber.xcframework.zip",
             checksum: "e8e939ae736599d5c221471d90afd9804c134607e7bc5a40e4cd25f37b197850"
         )
     ]
